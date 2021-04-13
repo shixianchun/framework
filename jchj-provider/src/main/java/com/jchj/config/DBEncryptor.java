538ddcfb26052638b6116eb4be45ed834a505c79
shixianchun <37069821@qq.com>
2021-04-13 12:00:12 +0800

    test dockerfile

--- jchj-provider/src/main/java/com/jchj/config/DBEncryptor.java
+++ jchj-provider/src/main/java/com/jchj/config/DBEncryptor.java
@@ -26,19 +26,19 @@ public class DBEncryptor {
         return encryptor;
     }
 
-    public static void main(String[] args) {
-        StringEncryptor dbEncryptor=new DBEncryptor().stringEncryptor();
-        String url = dbEncryptor.encrypt("jdbc:mysql://127.0.0.1:3306/jchj?characterEncoding=utf8&autoReconnect=true&useSSL=false");
-        String name = dbEncryptor.encrypt("root");
-        String password = dbEncryptor.encrypt("root");
-        System.out.println(url);
-        System.out.println(name);
-        System.out.println(password);
-        /**
-         ADM3ib+psARoGTbU2McRh14KCFld6Jg/IVLrBGaE2NBqqq8Jp5K7K+N0P4B45sdJ5NNpattdvRjMwkWsjT7ve8DrxTtQTTCKfvIwsoqBAMEFTWXMi2pRkKdhIwiVwFbs4QvBi58M58fTb3GGtA+jdNvztcG55KYOsTuCZJw12z8=
-         0jgsUFhtXHvCl7VN44yOV1jAa7de8Mw0R4Bol95RO7BZv/dIvvwcL2P9mxPdFuCV
-         DlC0lB7yZjTJfRZb7xaZ3q0LOBzwmxlLaJele2cIxpGKYIQCFgJuuaggX4z3aD4N
-         */
-    }
+//    public static void main(String[] args) {
+//        StringEncryptor dbEncryptor=new DBEncryptor().stringEncryptor();
+//        String url = dbEncryptor.encrypt("jdbc:mysql://192.168.110.140:3306/jchj?characterEncoding=utf8&autoReconnect=true&useSSL=false");
+//        String name = dbEncryptor.encrypt("root");
+//        String password = dbEncryptor.encrypt("root");
+//        System.out.println(url);
+//        System.out.println(name);
+//        System.out.println(password);
+//        /**
+//         NrkSmz/3s1cgdNvTFfU7mOQwtEFjodV9ysO/bpB6imbFYH6nShpDExWnzNCKGVpLcES+wZ6e8TEArq83EP8LC592VutW87lyISBa74V4QliuGH9kr/fGiGg00ELPqkc6BsbDVblKc9ZRiUnpu4HWQoHYsrngwFy3TkdjI2t0FEQ=
+//         LZmkNjmRG/4oce1AxSgvmofOPcyutyiB59hItqjdwTQPnMpQvns1zQ3PzX1xD8W3
+//         cJ7sH0s5t1x/uHxDoZyVotVrSOXr9iWrkKv4Nqgeo0vsQ1aa3H3Xzs5G/PhnKeYu
+//         */
+//    }
 
 }
